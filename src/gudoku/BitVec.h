ce857fec2069b082b72c478066e8eafb2cc20f04
XiongHui Guo <gz_shines@msn.com>
2022-03-12 21:26:30 +0800

    恢复 typedef BitVec16x16_AVX BitVec16x16;
    Signed-off-by: XiongHui Guo <gz_shines@msn.com>

--- src/gudoku/BitVec.h
+++ src/gudoku/BitVec.h
@@ -3920,7 +3920,7 @@ uint64_t whichIsNotDots64(const char * p) {
 }
 
 #if defined(__AVX2__) || defined(__AVX512VL__) || defined(__AVX512F__)
-typedef BitVec16x16_SSE     BitVec16x16;
+typedef BitVec16x16_AVX     BitVec16x16;
 #else
 typedef BitVec16x16_SSE     BitVec16x16;
 #endif // __AVX2__
